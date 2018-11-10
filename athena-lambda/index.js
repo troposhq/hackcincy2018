da9e8cf1bf0adada856831ea939fa81ed7da813f
Tyler Michael <tmich214@gmail.com>
2018-11-10 16:57:27 -0500

    write query to dynamo

--- athena-lambda/index.js
+++ athena-lambda/index.js
@@ -50,23 +50,47 @@ async function queryAthena(query, db, bucket) {
 
 function parseMetric(rs) {
   return new Promise((resolve, reject) => {
-    let metric = 0;
+    let metric = '';
     rs
       .pipe(csv()
       .on('data', (data) => {
-        const row = data.toString('utf8');
-        // TODO: parse row and set metric
-        console.log(row);
+        const row = JSON.parse(data.toString('utf8'));
+        // {"ga:newusers":"2"}
+        const columns = Object.keys(row);
+        if (columns.length !== 1) {
+          throw new Error("Queries must return only one column.");
+        }
+        metric = row[columns[0]];
       }))
       .on('done', () => resolve(metric))
       .on('error', reject);
   });
 }
 
-function upsertDynamo() {
+async function upsertDynamo(queryKey, metric) {
+  const dynamoClient = new AWS.DynamoDB.DocumentClient({
+    region: process.env.REGION,
+  });
+
+  const params = {
+    TableName: 'hackcincy2018_metrics',
+    Item: {
+      'metric': queryKey,
+      'value': metric
+    }
+  };
+
+  dynamoClient.put(params, function(err, data) {
+    if (err) {
+      console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+      console.log("Added item:", JSON.stringify(data, null, 2));
+    }
+  });
 }
 
 module.exports.handler = async (event, context, cb) => {
+  const queryKey = "new_users";
   // run query
   const query = process.env.ATHENA_QUERY;
   const db = process.env.ATHENA_DB;
@@ -74,4 +98,5 @@ module.exports.handler = async (event, context, cb) => {
   const athenaResult = await queryAthena(query, db, outputLocation);
   console.log(athenaResult);
   // upsert into dynamo
+  await upsertDynamo(queryKey, athenaResult);
 };
